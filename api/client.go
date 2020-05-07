55dd8df8696b25de1762ac08f2170136914eaecf
Mislav Marohnić <mislav@github.com>
2020-05-07 15:58:38 +0200

    Increase HTTP timeouts

--- api/client.go
+++ api/client.go
@@ -21,14 +21,14 @@ type ClientOption = func(http.RoundTripper) http.RoundTripper
 var DefaultTransport = &http.Transport{
 	Proxy: http.ProxyFromEnvironment,
 	DialContext: (&net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
+		Timeout:   90 * time.Second,
+		KeepAlive: 90 * time.Second,
 		DualStack: true,
 	}).DialContext,
 	ForceAttemptHTTP2:     true,
 	MaxIdleConns:          100,
 	IdleConnTimeout:       90 * time.Second,
-	TLSHandshakeTimeout:   10 * time.Second,
+	TLSHandshakeTimeout:   60 * time.Second,
 	ExpectContinueTimeout: 1 * time.Second,
 }
 
