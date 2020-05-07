df71194fc619305e81267f785bf03be300dbffc9
Mislav Marohnić <mislav@github.com>
2020-05-07 15:57:38 +0200

    Copy DefaultTransport from Go stdlib

--- api/client.go
+++ api/client.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/henvic/httpretty"
 )
@@ -16,9 +18,23 @@ import (
 // ClientOption represents an argument to NewClient
 type ClientOption = func(http.RoundTripper) http.RoundTripper
 
+var DefaultTransport = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+		DualStack: true,
+	}).DialContext,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
+
 // NewClient initializes a Client
 func NewClient(opts ...ClientOption) *Client {
-	tr := http.DefaultTransport
+	var tr http.RoundTripper = DefaultTransport
 	for _, opt := range opts {
 		tr = opt(tr)
 	}
